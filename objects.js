b60b6108bcf07b073e7e075d65f1cca0062cbcbf
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-04-02 12:47:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,11 @@
 var playlist = {artist:"song title"}
 
-function updatePlaylist(playlist, anotherArtist, anotherSong){
-  playlist[anotherArtist]=anotherSong;
+function updatePlaylist(playlist, artistName, anotherSong){
+  playlist[artistName]=anotherSong;
   return playlist;
 }
 
-function removeFromPlaylist(playlist)
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistname;
+  return playlist;
+}
